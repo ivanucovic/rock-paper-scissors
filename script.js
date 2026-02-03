216cf5a226594d65af12b1706375a59168578328
ivanucovic <ivan.ucovic10@gmail.com>
2026-02-03 21:44:59 +0100

    Finished game with reset button

--- script.js
+++ script.js
@@ -5,16 +5,13 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(getComputerChoice());
-
-function getHumanChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    return prompt("Enter your choice: Rock, Paper, or Scissors");
+function getHumanChoice(e) {
+    return e.target.textContent;
 }
 
-function playRound() {
+function playRound(humanChoice) {
     const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
+    const humanSelection = humanChoice;
     if (computerSelection === humanSelection) {
         return "Tie";
     } else if (computerSelection === "Rock" && humanSelection === "Scissors") {
@@ -28,24 +25,58 @@ function playRound() {
         return "Computer Wins";    
     } else {
         scoreHuman++;
-        return "Human Wins";
+        return "Player Wins";
     }
+}
+
+function updateRound() {
+    document.querySelector("#player-score").textContent = "Player Score: " + scoreHuman;
+    document.querySelector("#computer-score").textContent = "Computer Score: " + scoreComputer;
+    document.querySelector("#round-result").textContent = "Round Result: " + result;
+}
+
+function gameWin() {
+    document.querySelector("#game-winner").textContent = "Game Winner: " + gameWinner;
+    const body = document.querySelector("body")
+    resetButton = document.createElement("button")
+    resetButton.textContent = "Reset"
+    body.appendChild(resetButton)
+    resetButton.addEventListener("click", () => {
+        scoreComputer = 0;
+        scoreHuman = 0;
+        result = "";
+        gameWinner = "";
+        document.querySelector("#player-score").textContent = "Player Score: " + scoreHuman;
+        document.querySelector("#computer-score").textContent = "Computer Score: " + scoreComputer;
+        document.querySelector("#round-result").textContent = "Round Result: " + result;
+        document.querySelector("#game-winner").textContent = "Game Winner: " + gameWinner;
+    })
 }
 
 let scoreComputer = 0;
 let scoreHuman = 0;
+let result = "";
+let gameWinner = "";
+document.querySelector("#round-results").addEventListener('update', updateRound)
+document.querySelector("#game-winner").addEventListener('winner', gameWin)
 
-for (i = 0; i < 5; i++) {   
-    console.log(playRound());
-    console.log("Computer Score: " + scoreComputer);
-    console.log("Human Score: " + scoreHuman);
-}
-
-if (scoreComputer > scoreHuman) {
-    console.log("Computer Wins");
-} else if (scoreHuman > scoreComputer) {
-    console.log("Human Wins");
-} else {
-    console.log("Tie");
-}
-
+document.querySelector("#buttons").addEventListener("click", (e) => {
+    console.log(e);
+    let choice = getHumanChoice(e);
+    console.log(choice);
+    result = playRound(choice);
+    document.querySelector("#round-results").dispatchEvent(new Event('update'));
+    if (scoreComputer >=3 || scoreHuman >= 3) {
+        if (scoreComputer > scoreHuman) {
+        console.log("Computer Wins");
+        gameWinner = "Computer";
+    } else if (scoreHuman > scoreComputer) {
+        console.log("Player Wins"); 
+        gameWinner = "Player"; 
+    } else {
+        console.log("Tie");
+        gameWinner = "Tie";
+    } 
+    document.querySelector("#game-winner").dispatchEvent(new Event('winner'));
+    }
+})
